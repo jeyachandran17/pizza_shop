fdea541ae28db7e34ec0666484ba246ad1f608b1
jeyachandran17 <jeyachandran1733@gmail.com>
2025-08-08 22:18:25 +0530

    create home page

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function Application() {
       <div className='main_container'>
         <h3>Welcome to the Application</h3>
         <Navigation />
+        <Home_page />
       </div>
   )
 }
@@ -40,6 +41,36 @@ function Navigation() {
     </nav>
   )
 }
+
+function Home_page() {
+  return (
+    <div className="homepage_Main-container">
+      <div className="homepage_container1">
+        <div className='home_page1'>
+          <p>Welcome to Our Pizza Place</p>
+        </div>
+        <div className='home_page2'>
+          <h3>Delicious pizzas made with love and fresh ingredients.</h3>
+        </div>
+        <div className='home_page3'>
+          <p>A small river named Duden flows by their place and supplies it with the necessary regelialia</p>
+        </div>
+        <div className='home_page4'>
+          <button>View menu</button>
+        </div>
+      </div>
+      <div className="homepage_container2">
+        {/* In future replace the loge at the "pizza" text  */}
+        <h3>WELCOME TO PIZZA RESTAURANT</h3>
+        {/* then at more contant or discribe the restaurant */}
+      </div>
+    </div>
+  )
+}
+
+
+
+
 
 
 export default App
